8b9410c36ca347ba25fa5bd16062332b0ff4a34d
Nathan <pythoniszen@gmail.com>
2024-12-18 21:40:19 -0800

    Add logic to exit program

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
   char input[100];
 
   while(1) {
+    /* Displays shell prompt, forces prompt to display before program exits
+     * Gets user input with max of 100 char 
+     */
     printf("shell>");
     fflush(stdout);
-
     fgets(input, 100, stdin);
+    
+    /* Removes newline kept from fgets
+     * If user input was "exit", break the loop leave the program
+     */
+    input[strcspn(input, "\n")] = 0;
+    if (strcmp(input, "exit") == 0) {
+      break;
+    }
+
     printf("You: %s", input);
   }
+
   return 0;
 }
