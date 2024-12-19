1ad4b0633cc6869eff76d19147a6c202fe04c503
Nathan <pythoniszen@gmail.com>
2024-12-19 02:10:01 -0800

    Create array of cli arguments and print tokens

--- main.c
+++ main.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+/* Simple shell program which reads user input,
+ * tokenizes commands, and implements program exit command
+ */
 int main() {
   char input[100];
 
   while(1) {
-    /* Displays shell prompt, forces prompt to display before program exits
-     * Gets user input with max of 100 char 
-     */
+    /* Display shell prompt and collect user input */
     printf("shell>");
     fflush(stdout);
     fgets(input, 100, stdin);
     
-    /* Removes newline kept from fgets
-     * If user input was "exit", break the loop leave the program
-     */
+    /* Remove newline and implement exit command */
     input[strcspn(input, "\n")] = 0;
     if (strcmp(input, "exit") == 0) {
       break;
     }
 
-    printf("You: %s", input);
+    /* Create array of pointers to store command line arguments */
+    char *clargs[20];
+    char *token = strtok(input, " ");
+    int i = 0;
+
+    /* Create array of cli arguments */
+    while(token != NULL && i < 19) {
+      clargs[i] = token;
+      token = strtok(NULL, " ");
+      i++;
+    }
+    clargs[i] = NULL;
+
+    /* Test print tokens */
+    printf("Tokens:\n");
+    for(int j = 0; clargs[j] != NULL; j++) {
+      printf("arg: %s\n", clargs[j]);
+    }
   }
 
   return 0;
